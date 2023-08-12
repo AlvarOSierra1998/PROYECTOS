4a5364004389125895cbd0bdaee2765171f58f29
AlvarOSierra1998 <127144562+AlvarOSierra1998@users.noreply.github.com>
2023-08-12 15:37:07 +0200

    Add files via upload
    Pagina con amigos

---
+++ BUJERO(1)/BUJERO/js/musica.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var miSonido = document.getElementById("miSonido");
+    if (miSonido) {
+        miSonido.play();
+    }
+});
+$(document).ready(function() {
+    var miSonido = document.getElementById("miSonido");
+    miSonido.play();
+});
